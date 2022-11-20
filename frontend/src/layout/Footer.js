750aebadee24f9790740d8632584b788981c6248
Desai Aayushi <46705941+desai-aayushi@users.noreply.github.com>
2022-11-20 15:57:42 -0500

    Add files via upload

---
+++ frontend/src/layout/Footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {  
+    render() {  
+        return (  
+            <div>
+                <footer>
+                    <div className="copy-right">
+                        <div className="container">
+                        </div>
+                    </div>
+                </footer>
+            </div>  
+        )  
+    }  
+}  
+  
+export default Footer
