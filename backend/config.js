bdb0adb588f0cae78fded1bb959e2465694c2370
Desai Aayushi <46705941+desai-aayushi@users.noreply.github.com>
2022-11-20 15:50:08 -0500

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+  PORT: process.env.PORT || 5000,
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://5Vi0zTnVsxVgnXVS:5Vi0zTnVsxVgnXVS@cluster0.lj5e9.mongodb.net/book_store?retryWrites=true&w=majority',
+  JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
+  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
+  accessKeyId: process.env.accessKeyId || 'AKIAJHDSE73P3D5AD4GQ',
+  secretAccessKey: process.env.secretAccessKey || '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',
+};
---
+++ backend/models/shopModel.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+
+const shopSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  image: { type: String },
+  owner: { type: String },
+  phone: { type: String },
+  description: { type: String },
+  address: { type: String, required: true },
+});
+
+const shopModel = mongoose.model('Shop', shopSchema);
+
+export default shopModel;
